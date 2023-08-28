11ba6a3ec192c91f924c0b08523278ae1f6ff0ac
Jose Henrique <jh4832184@gmail.com>
2023-08-28 09:35:37 -0300

    inserir em lista terminado

--- Estrutura de dados/Lista.c
+++ Estrutura de dados/Lista.c
@@ -17,6 +17,7 @@ typedef struct nodo {
   struct nodo *proximo;
 }Nodo;
 
+//lista de contatos
 typedef struct lista{
   Nodo *inicio;
 }Lista;
@@ -27,7 +28,54 @@ Lista * criarLista() {
   return list;
 }
 
-void inserir(Lista *list, char nome[100], char telefone[20]){}
+void inserir(Lista *list, char nome[100], char telefone[20])
+{
+  Nodo * novo = (Nodo*)malloc(sizeof(Nodo));
+  novo->anterior = NULL;
+  (*novo).proximo = NULL;
+
+  //copiando a string recebida dentro da estrutura contato
+  strcpy(novo->contato.nome, nome);
+  strcpy(novo->contato.telefone, telefone);
+
+  if(list->inicio == NULL)
+  {
+    list->inicio = novo;
+  }
+
+  else 
+  {
+    Nodo *aux = list->inicio;
+    int comp = strcmp(nome, aux->contato.nome);
+    while (comp>0 && aux->proximo!=NULL)
+    {
+      aux = aux->proximo;
+      comp = strcmp(nome, aux->contato.nome);
+    }
+
+    if (comp>0)
+    {
+      aux->proximo = novo;
+      novo->anterior = aux;
+    }
+
+    else if (list->inicio == aux)
+        {
+          aux->anterior = novo;
+          novo->proximo = aux;
+          list->inicio = novo;
+        }
+
+        else
+        {
+          novo->anterior = aux->anterior;
+          novo->proximo = aux;
+          aux->anterior->proximo = novo;
+          aux->anterior = novo;
+        }
+    
+  }
+}
    
 
 
