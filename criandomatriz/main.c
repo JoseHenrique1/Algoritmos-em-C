6f80c2c552a4b806bcf17bc0cfadca4118fe0826
Jose Henrique <jh4832184@gmail.com>
2023-09-05 15:39:13 -0300

    corrigindo matriz

--- criandomatriz/main.c
+++ criandomatriz/main.c
@@ -26,6 +26,8 @@ void imprimir_matriz(int vet[]) {
             printf("\n");
         }
     }
+
+    printf("\n");
 }
 
 void inserir_elemento(int vet[], int num, int linha, int coluna) {
@@ -48,16 +50,20 @@ int buscar_elemento(int vet[], int linha, int coluna) {
 
 }
 
-void soma_matriz (int vet1[], int vet2[], int vet3[]) {
-  int k;
-  for (k=0; k<tamanho; k++) {
-    vet3[k] = vet1[k] + vet2[k]; 
+void soma_matriz(int vet1[], int vet2[], int vet3[]) {
+  int i, j;
+  for (i = 1; i <= max_linha; i++) {
+    for (j = 1; j <= max_coluna; j++) {
+      int val1 = buscar_elemento(vet1, i, j);
+      int val2 = buscar_elemento(vet2, i, j);
+      int soma = val1 + val2;
+      inserir_elemento(vet3, soma, i, j);
+    }
   }
-    
 }
 
 void main() {
-    dimensionar_matriz(3,3);
+    dimensionar_matriz(2,2);
 
     //vetor que simula a matriz
     int vet1[tamanho];
@@ -67,23 +73,20 @@ void main() {
     zerar_matriz(vet1);
     zerar_matriz(vet2);
     zerar_matriz(vet3);
-    printf("\n\n");
 
     inserir_elemento(vet1, 5, 1, 1);
     inserir_elemento(vet1, 5, 1, 2);
-    inserir_elemento(vet1, 5, 1, 3);
     
     inserir_elemento(vet2, 5, 1, 1); 
     inserir_elemento(vet2, 5, 1, 2);
-    inserir_elemento(vet2, 5, 1, 3);
 
-    
 
     
     soma_matriz(vet1, vet2, vet3);
 
+    imprimir_matriz(vet1);
+    imprimir_matriz(vet2);
     imprimir_matriz(vet3);
-
     printf("\n\n Buscando na posicao 1 - 1:  ");
     int num = buscar_elemento(vet1, 1, 1);
     printf("%d \n", num);
